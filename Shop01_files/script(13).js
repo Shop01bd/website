86784b9c2a313673d03135cb01283a54a3116293
Moin Uddin Hasan <query.shop01@gmail.com>
2020-12-21 15:54:32 +0600

    initial commit

---
+++ Shop01_files/script(13).js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-16',
+          componentName:'@widget/LAYOUT/bs-ActionText-Component',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:"@widget/LAYOUT/bs-ActionText-Component"
+        });
+      })({"id":"n-36873723-membership-header","text":"Signed in as:","style":{"dropdown":{"position":"absolute","right":"0px","top":"initial","whiteSpace":"nowrap","maxHeight":300,"overflowY":"auto","display":"none","zIndex":"3","@md-only":{"right":"0px"},"@sm-only":{"right":"0px"},"width":240},"listItem":{"display":"block","textAlign":"left","marginBottom":"0"},"separator":{"marginTop":"small","marginBottom":"small"},"marginBottom":"small","font":"alternate","fontWeight":"bold"},"widgetId":"e6c237ce-01e0-43f3-9642-30dcc662446d","section":"default","category":"accent","locale":"en-IN","renderMode":"PUBLISH"},{"widgetId":"e6c237ce-01e0-43f3-9642-30dcc662446d","widgetType":"HEADER","widgetPreset":"header9","section":"default","category":"accent","fontSize":"medium","fontFamily":"alternate","group":"UtilitiesMenu","groupType":"Menu","websiteThemeOverrides":{},"widgetThemeOverrides":{}});
---
+++ Shop01_files/script(16).js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-19',
+          componentName:'@widget/LAYOUT/bs-Search-Component',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:"@widget/LAYOUT/bs-Search-Component"
+        });
+      })({"isShopPage":false,"shopPageId":"9254a694-d5f4-4fb8-a0b3-c1f692f40118","shopRoute":"\u002Fshop","inNavigationDrawer":true,"staticContent":{"hamburgerIcon":"Hamburger Site Navigation Icon","YouTube":"YouTube Social Link","cartProducts":"Products","Facebook":"Facebook Social Link","carouselAddImageHelper":"Click Here to Add Images to Slideshow!","membershipSectionTitle":"Account","membershipSignedInStatus":"Signed in as:","Xing":"Xing Social Link","membershipSignedOutStatus":"Currently not signed in","Houzz":"Houzz Social Link","morePages":"More","membershipBookings":"Bookings","Yelp":"Yelp Social Link","Google+":"Google Plus Social Link","Instagram":"Instagram Social Link","Twitter":"Twitter Social Link","translateBarHelper":"Translate: ","promoBannerLink":"Promotion Banner Link","membershipAccount":"My Account","membershipSignIn":"Sign In","navigationDropdownToggle":"Website navigation dropdown menu","cartIcon":"Shopping Cart Icon","Pinterest":"Pinterest Social Link","membershipDropdownToggle":"Website member dropdown menu","membershipCreateAccount":"Create Account","membershipOrders":"Orders","membershipSignOut":"Sign out","membershipSignOutCaps":"SIGN OUT","search_placeholder":"Search Products","LinkedIn":"LinkedIn Social Link","cartServices":"Services"},"domainName":"shop01bd.com","pageRoute":"\u002F","keepOpen":true,"searchFormLocation":"NAV_DRAWER","widgetId":"e6c237ce-01e0-43f3-9642-30dcc662446d","section":"default","category":"neutral","locale":"en-IN","renderMode":"PUBLISH"},{"widgetId":"e6c237ce-01e0-43f3-9642-30dcc662446d","widgetType":"HEADER","widgetPreset":"header9","section":"default","category":"neutral","fontSize":"medium","fontFamily":"alternate","group":"NavigationDrawer","groupType":"Default","websiteThemeOverrides":{},"widgetThemeOverrides":{}});
